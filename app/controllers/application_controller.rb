7502faf6996b8c69d0b8a2560d2f02100ee977f8
Matthew Oppenheimer <mattopps@gmail.com>
2016-03-24 12:18:54 -0400

    add current user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :ensure_current_user, except: [:index, :show]
+  helper_method :current_user
+
+  def current_user
+    user = USer.find_by(id: session[:user_id]) if session[:user_id]
+  end
 
   private
 
   def ensure_current_user
+    redirect_to new_session_path unless current_user
   end
+
 end
