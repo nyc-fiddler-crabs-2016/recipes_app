281b7a89312869b7858bdd0e2b8aa1989c509ac6
Matthew Oppenheimer <mattopps@gmail.com>
2016-03-24 12:17:04 -0400

    add sessions controller and bfore action to ensure login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :ensure_current_user, except: [:index, :show]
+
+  private
+
+  def ensure_current_user
+  end
 end
