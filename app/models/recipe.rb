3249238c9e565557cfee8c97b736d539c590752e
Matthew Oppenheimer <mattopps@gmail.com>
2016-03-24 11:54:00 -0400

    Fix typo in association

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,5 @@ class Recipe < ActiveRecord::Base
   has_many :user_recipes
   has_many :users, through: :user_recipes
   has_many :recipe_ingredients
-  has_many :ingredients, through: :recipes_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
