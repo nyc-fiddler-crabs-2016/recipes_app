16e576423eaae71d50ad2508c5ed40470f33a2b6
Matthew Oppenheimer <mattopps@gmail.com>
2016-03-24 11:06:10 -0400

    create models and migrations. create databse and migrate

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :recipe do
+    name "MyString"
+    type ""
+    description "MyString"
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    username "MyString"
+     "MyString"
+    password_digest "MyString"
+    email "MyString"
+  end
+end
